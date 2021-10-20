6d048c2d2f8a2fe10c2d33ff14c5e1abc9b4d6aa
joaogmelo95 <joaogmelo95@gmail.com>
2021-10-20 19:17:53 -0300

    Requisito 1 - v2 - ajustando ao lint

--- script.js
+++ script.js
@@ -24,34 +24,33 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 async function addItemsToScreen() {
   const data = await fetchProducts('computador');
-  const results = data.results;
 
-  results.forEach((r) => {
+  data.results.forEach((r) => {
     const itemsSection = document.querySelector('.items');
     const sku = r.id;
     const name = r.title;
     const image = r.thumbnail;
-    const section = createProductItemElement({sku, name, image});
+    const section = createProductItemElement({ sku, name, image });
     itemsSection.appendChild(section);
-  })
+  });
 }
 
 window.onload = () => {

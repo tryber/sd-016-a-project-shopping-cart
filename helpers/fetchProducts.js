8303609822e892c30215f194b44861c4e44e16cd
arctavares <cosilk@hotmail.com>
2021-10-20 20:06:33 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,10 +1,13 @@
 const fetchProducts = async (search) => {
+  if (search === undefined) {
+    throw new Error('You must provide an url');
+  }
 
   const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
-    .then(data => data.json())
-    .catch(error => console.log(error));
+    .then((data) => data.json())
+    .catch((error) => error);
 
-    return result;
+  return result;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -12,7 +12,7 @@ describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
   const parameterPC = 'computador';
   it('Test if fetchProducts is a function', () => {
-    expect(typeof(fetchProducts)).toBe('function');
+    expect(typeof (fetchProducts)).toBe('function');
   })
   it('Test if fetchProducts(computador) calls fetch', async () => {
     fetchProducts(parameterPC);
@@ -22,11 +22,19 @@ describe('1 - Teste a função fecthProducts', () => {
     fetchProducts(parameterPC);
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   })
-  
-  it('Test if fetchProducts(computador) equals to computadorSearch',async () => {
+
+  it('Test if fetchProducts(computador) equals to computadorSearch', async () => {
     const products = await fetchProducts(parameterPC);
     expect(products).toBe(computadorSearch);
-  
+  })
+
+  it('Test if fetchProducts() equals to error(You must provide an url)', async () => {
+    try {
+      await fetchProducts();
+    } catch (e) {
+      expect(e).toEqual(new Error('You must provide an url'));
+    }
+
   })
 
 });

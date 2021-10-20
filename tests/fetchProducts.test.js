320b34e21a41ba16262b578473a845d714e49044
cdartora <cdartora>
2021-10-20 16:05:23 -0300

    test(fetchProducts): testa se é uma função e se fetch é chamado

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,15 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('Testa se fetchProducts é uma função', () => {
+
+    expect(typeof fetchProducts).toBe('function');
+  })
+
+  it('Executa a função fetchProducts com o argumento "computador" e testa se fetch foi chamada', () => {
+
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  })
 });

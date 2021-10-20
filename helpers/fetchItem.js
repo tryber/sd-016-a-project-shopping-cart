c9d43a3eea72d19c1d9999656f014bf0c3a49d84
alt-art <pedromendescraft@gmail.com>
2021-10-20 19:19:34 -0300

    Fix lint error in fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,7 +1,6 @@
-const fetchItem = (id) => {
-  return fetch(`https://api.mercadolibre.com/items/${id}`)
+const fetchItem = (id) =>
+  fetch(`https://api.mercadolibre.com/items/${id}`)
     .then((res) => res.json());
-};
 
 if (typeof module !== 'undefined') {
   module.exports = {

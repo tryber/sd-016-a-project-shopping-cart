db4633f82bb52ebd0f3eb3eacb9e0347d569894c
joaoOliveira <joaoavelino2008@hotmail.com>
2021-10-20 18:12:20 -0300

    fix: lint

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,4 +1,4 @@
-const fetchProducts = async (arg) => {
+const fetchProducts = async (arg, callback) => {
   // seu código aqui
   try {
     const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${arg}`);
@@ -7,12 +7,12 @@ const fetchProducts = async (arg) => {
     await data.results.forEach((element) => {
     const { id, title, thumbnail } = element;
     console.log(id, title, thumbnail);
-    itemsSection.appendChild(createProductItemElement({ id, title, thumbnail }));
+    itemsSection.appendChild(callback({ id, title, thumbnail }));
     });
-  } catch(error) {
-    window.alert('404 NOT FOUND');
+  } catch (error) {
+    window.alert('404 NOT FOUND');  
   }
-}
+};
 
 if (typeof module !== 'undefined') {
   module.exports = {

1709ed304b6c87641b19cb318e466feb562669db
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-10-20 16:52:48 -0300

    Implementa a funcao fetchProducts. fetchProducts.test - testa se fetchProducts é uma função

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,10 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const dataResult = fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.results)
+    .catch(() => new Error('You must provide an url'));
+  return dataResult;
 };
 
 if (typeof module !== 'undefined') {

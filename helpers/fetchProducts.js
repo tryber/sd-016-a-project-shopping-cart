1721e20cfe4851d67538e22526b5bcaf522d4146
Filipe Ferreira Paulo <filipef.paulo@gmail.com>
2021-10-20 17:08:32 -0300

    feat: adiciona a função fetch responsável por buscar os dados no banco de dados

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,13 @@
-const fetchProducts = () => {
+const getUrl = (productName) => `https://api.mercadolibre.com/sites/MLB/search?q=${productName}`;
+
+const fetchProducts = async (productName) => {
   // seu código aqui
+  const url = getUrl(productName);
+
+  const productSearch = await fetch(url);
+  const product = await productSearch.json();
+
+  return product;
 };
 
 if (typeof module !== 'undefined') {

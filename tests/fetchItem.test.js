fec0664e0dfb699c9b316890419352ab96ae0a6f
LHenriquerios <henriquerios4@gmail.com>
2021-10-20 14:26:47 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,10 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
+  const sum = (a, b) => a + b;
+
+test('sums two values', () => {
+  expect(sum(2, 3)).toEqual(5);
+});
   fail('Teste vazio');
 });

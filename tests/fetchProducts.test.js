9ad400e33ab1c9f72a99bc5b2d7c024fe031f57f
Jose <josetoitio@gmail.com>
2021-10-20 19:14:57 -0300

    Testes 3 e 4 completos

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,5 +8,20 @@ describe('1 - Teste a função fecthProducts',  () => {
   it('Testa se a função fecthProducts é uma função', async () => {
     const typeOfFecthProducts = await typeof fetchProducts;
     expect(typeOfFecthProducts).toEqual('function');
-  })
+  });
+
+  it('Testa se o fetchProducts foi chamado', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Testa se o endpoint foi chamado', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/sites/MLB/search?q=computador")
+  });
+
+  it('Testa se o resultado de fetchProducts é igual ao computadorSearch', async () => {
+    await fetchProducts('computador')
+    .then((data) => expect(data).toEqual(computadorSearch))
+  });
 });

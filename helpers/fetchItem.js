0538affbb5299cd2d060899b85edfacb5f785668
CamilaRanniele <rannielecamsil@gmail.com>
2021-10-20 20:49:38 -0300

    Requisito 2

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,4 +1,8 @@
 const fetchItem = (object) => {
+  if (!object) {
+    const erro = new Error('You must provide an url');
+    return erro.message;
+  }
   const url = `https://api.mercadolibre.com/items/${object}`;
   return fetch(url)
     .then((response) => response.json())
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,7 +1,6 @@
 const fetchSimulator = require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
-//const { fetchItem } = require('../helpers/fetchItem');
 
 window.fetch = jest.fn(fetchSimulator);
 

7883e68725d288bb014f1f1231ce82432a76de6a
arctavares <cosilk@hotmail.com>
2021-10-20 16:48:24 -0300

    Add function and test 1, 2, 3 - Task 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,11 @@
-const fetchProducts = () => {
+const fetchProducts = (search) => {
   // seu código aqui
-  
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${search}`;
+
+  fetch(API_URL)
+    .then(data => data.json())
+    .then(object => object.results)
+    .catch(error => console.log(error));
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,7 +4,28 @@ const computadorSearch = require('../mocks/search');
 
 window.fetch = jest.fn(fetchSimulator);
 
+beforeEach(() => {
+  fetch.mockClear();
+});
+
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  const parameterPC = 'computador';
+  it('Test if fetchProducts is a function', () => {
+    expect(typeof(fetchProducts)).toBe('function');
+  })
+  it('Test if fetchProducts(computador) calls fetch', async () => {
+    fetchProducts(parameterPC);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  })
+  it('Test if parameter is called in the function', () => {
+    fetchProducts(parameterPC);
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  })
+  /*
+  it('Test if fetchProducts(computador) equals to computadorSearch', () => {
+    const functionCall = fetchProducts(parameterPC);
+    expect(fetchProducts(functionCall)).toBe(computadorSearch);
+  })
+  */
 });

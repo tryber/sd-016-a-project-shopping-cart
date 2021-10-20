ccfd57f3fc91065d6e8941bc979f7c2dcd452937
Marcelo Oliveira <celocmoliveira@gmail.com>
2021-10-20 15:02:07 -0300

    Iniciando projeto VQV!

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,6 +5,6 @@ const computadorSearch = require('../mocks/search');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+ 
+  // fail('Teste vazio');
 });

5e75de2d86c77554ae74d5d9ff9f8520f1c9af0e
alt-art <pedromendescraft@gmail.com>
2021-10-20 17:54:09 -0300

    Test check if the fetch was called with the correct endpoint

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -14,6 +14,11 @@ describe('1 - Teste a função fecthProducts', () => {
     expect(fetch).toHaveBeenCalled();
   });
 
+  it('se o fetch foi chamado com o enpoint', () => {
+    fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/sites/MLB/search?q=computador");
+  });
+
   it("fetchProducts retorna o resultado esperado quando pesquisa por \"computador\"", () => {
     fetchProducts("computador")
       .then((res) =>

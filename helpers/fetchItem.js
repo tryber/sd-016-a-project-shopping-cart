452a7e8ea11164b2138d00f9db3219a69212346a
cdartora <cdartora>
2021-10-20 19:44:40 -0300

    feat(fetchItem): implementa fetch api de item único

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,17 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (id) => {
+  const endpoint = `https://api.mercadolibre.com/items/${id}`;
+
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    
+    console.log(data);
+  } catch (error) {
+    console.log('Erro na função fetchItem: ', error);
+  }
 };
+
+fetchItem('MLB1341706310');
 
 if (typeof module !== 'undefined') {
   module.exports = {

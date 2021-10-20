084e991074b98583dcae9815dc297767a836b4c5
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-10-20 17:04:15 -0300

    Executa a função fetchProducts com o argumento "computador" e testa se "fetch" foi chamada

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,10 +4,16 @@ const computadorSearch = require('../mocks/search');
 
 window.fetch = jest.fn(fetchSimulator);
 
-describe('1 - Teste a função fecthProducts', () => {
+describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  it ('Testa se fetchProducts é uma função', async () => {
+  it('Testa se fetchProducts é uma função', async () => {
     expect.assertions(1);
     expect(fetchProducts).toBeInstanceOf(Function);
-  })
+  });
+
+  it('Executa a função fetchProducts com o argumento "computador" e testa se "fetch" foi chamada', async () => {
+    expect.assertions(1);
+    await fetchProducts(computadorSearch);
+    expect(fetch).toHaveBeenCalled();
+  });
 });

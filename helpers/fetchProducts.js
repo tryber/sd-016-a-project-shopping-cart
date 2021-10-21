9c4c7c03975fd003b031874f7d08a03315d356e1
Fernanda9421 <fer.candrade21@gmail.com>
2021-10-21 07:59:50 -0300

    Implementa funçao fetchProducts - Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (productName) => {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${productName}`;
+    const product = await fetch(url);
+    const response = await product.json();
+    return response.results;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {

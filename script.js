6c0d6f11fd0c414146cff6d47ca003014b1d60ca
lonardow <lonardow_fg@hotmail.com>
2021-10-21 18:34:01 -0300

    requisito 3 com a ajuda do josue

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function createCustomElement(element, className, innerText) {
 }
 
 function cartItemClickListener(event) {
+  event.target.remove(event);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {

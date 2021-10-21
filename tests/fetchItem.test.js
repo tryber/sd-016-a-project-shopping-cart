b4dd452536ea5981ace2c438aaafd17210ec0168
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-10-21 10:34:42 -0300

    teste 2 do requisito 2

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,7 +6,11 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
   // implemente seus testes aqui
+  fetchItem("MLB1615760527");
   it('1 - Teste se fetchItem é uma função', () => {
     expect(typeof fetchItem).toBe('function');
   })
+  it('2 - Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', () => {
+    expect(fetch).toHaveBeenCalled();
+  });
 });

a34c3353f87380741cbcbe281953924518f7b404
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-10-21 18:07:37 -0300

    fix: correcao da funcao fetchProducts, incluindo condicao de caso de ERRO

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,9 @@
 const mercadoLibreUrl = (querry) => `https://api.mercadolibre.com/sites/MLB/search?q=${querry}`;
 
 const fetchProducts = async (querryUserInfo) => {
+  if (!querryUserInfo) {
+    throw new Error('You must provide an url');
+  }
   const url = mercadoLibreUrl(querryUserInfo);
   const response = await fetch(url);
   const data = await response.json();

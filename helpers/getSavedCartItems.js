12a2bfb205b368a536c643f1b6051d4198c06011
Adran <adran.carnavale@gmail.com>
2021-10-21 18:54:49 -0300

    feat: adds teste to function getSavedCartItems

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,4 +1,4 @@
-const getSavedCartItems = () => localStorage.getItem('cartItem');
+const getSavedCartItems = () => localStorage.getItem('cartItems');
 
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;

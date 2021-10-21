05c4e4ea390220ff02a50ef896431aa6e8ff59c7
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-10-20 21:08:36 -0300

    feat: adicionou tests das funcoes fetchProducts e FetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,41 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  test('1- check if fetchItem is a function', async () => {
+    expect(typeof (fetchItem)).toBe('function');
+  });
+
+  test('2- check if it have a call for fetch on fetchItem with "MLB1615760527" argument ', async () => {
+
+    const response = await fetchItem('MLB1615760527');
+
+    expect(typeof (fetchItem(response))).toBe('object');
+
+  });
+
+  test('3 -check if fetchItem with "MLB1615760527" argument  return the endpoint "https://api.mercadolibre.com/items/MLB1615760527""', async () => {
+
+    const response = await fetchItem('MLB1615760527');
+    const { id } = response;
+
+    expect(id).toBe('MLB1615760527');
+  });
+
+  test('4- check if fetchItem function with "MLB1615760527" argument returns as same as object item', async () => {
+
+    const response = await fetchItem('MLB1615760527');
+    expect(response).toBe(item);
+
+  });
+
+  test('5- check if fetchProducts function with no argument returns the error "You must provide an url" ', async () => {
+
+    try {
+      await fetchItem();
+    } catch (error){
+    expect(error).toEqual(new Error('You must provide an url'));
+  }
+  });
+
 });

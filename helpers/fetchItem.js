7bea1f89b30bee4d00430375a03ff21988663f64
Laura <lahfumagalli@gmail.com>
2021-10-20 22:33:14 -0300

    fix lint

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,7 +1,10 @@
-const fetchItem = () => {
+const fetchItem = async (id) => {
   // seu código aqui
+  const itemId = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const itemIdJson = await itemId.json();
+  console.log(itemIdJson);
 };
-
+fetchItem('MLB1341706310');
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchItem,
--- script.js
+++ script.js
@@ -41,11 +41,12 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 const funcFet = () => {
-  fetchProducts('computador').then((promisse) => promisse.forEach(({id, title, thumbnail}) => {
-    const obj = { sku: id, name: title, image: thumbnail};
-    document.querySelector('.items').appendChild(createProductItemElement(obj))}));
-}
+  fetchProducts('computador').then((promisse) => promisse.forEach(({ id, title, thumbnail }) => {
+    const obj = { sku: id, name: title, image: thumbnail };
+    document.querySelector('.items').appendChild(createProductItemElement(obj));
+  }));
+};
 
 window.onload = () => {
-  funcFet()
+  funcFet();
 };

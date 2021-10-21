ef73aff2840f364453e592f7dee5cd76db5a5a54
alt-art <pedromendescraft@gmail.com>
2021-10-20 21:47:53 -0300

    Impl items can be placed in cart list

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const sectionItems = document.querySelector('.items');
+const olCart = document.querySelector('.cart__items');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,22 +15,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
@@ -40,6 +25,27 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  const itemAdd = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  itemAdd.addEventListener('click', () =>
+    fetchItem(sku).then((item) =>
+      olCart.appendChild(createCartItemElement(
+        { sku: item.id, name: item.title, salePrice: item.price },
+      ))));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(itemAdd);
+
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 window.onload = () => {

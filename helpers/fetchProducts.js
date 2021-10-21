1871da4788b13dde77308ed106ad78b711c4d282
Marcelo dos Santos Costa <marcelodscosta@yahoo.com.br>
2021-10-21 18:34:27 -0300

    Requisito 1 - Ajuste (Cobertura de Testes)

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,15 +1,16 @@
-const fetchProducts = async (produto) => {
-  try {
-    const response = await fetch(
-      `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`,
-    );
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    return new Error('You must provide an url');
-  }
-};
-
+const fetchProducts = (produto) =>
+  // try {
+  //   const response = await fetch(
+  //     `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`,
+  //   );
+  //   const json = await response.json();
+  //   return json;
+  // } catch (error) {
+  //   return new Error('You must provide an url');
+  // }
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produto}`)
+    .then((response) => response.json())
+    .then((json) => json);
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,

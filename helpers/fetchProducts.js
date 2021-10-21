8c06f76acc24cce011d63257ee42f5b7877c9da5
MatheusBeniniF <matheus_benini@hotmail.com>
2021-10-21 19:32:27 -0300

    Requisito 1 faltando 1 teste

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,8 @@
-const fetchProducts = () => {
-  
+const fetchProducts = (query) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+  .then((response) => response.json())
+  .then((data) => console.log(data.results))
+  .catch((error) => error);
 };
 
 if (typeof module !== 'undefined') {

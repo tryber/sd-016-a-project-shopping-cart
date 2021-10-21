2f3ec1139f55430321338357e7ca7cb0d47c79f7
Kazuo-Abduch <kazuo.abduch@gmail.com>
2021-10-20 22:00:50 -0300

    Refatoração - linha redundante

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,7 +2,6 @@ const fetchItem = (itemID) =>
   // seu código aqui
   fetch(`https://api.mercadolibre.com/items/${itemID}`)
    .then((response) => response.json())
-   .then((data) => data)
    .catch(() => {
     const newError = 'You must provide an url';
     return newError;
--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,7 +2,6 @@ const fetchProducts = (search) =>
   // seu código aqui
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
    .then((response) => response.json())
-   .then((data) => data)
    .catch(() => {
     const newError = 'You must provide an url';
     return newError;

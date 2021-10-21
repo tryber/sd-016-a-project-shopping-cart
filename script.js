5bd4e56361249710703a7668e5fdc389fd746d61
Veidz <joaovictorveidz@gmail.com>
2021-10-21 01:42:09 -0300

    3 - Removendo o item do carrinho ao selecionar nele

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ id: sku, name, salePrice }) {

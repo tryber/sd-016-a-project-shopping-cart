5333086a2ddd89b3fdea38b10c63bc4e3c819963
joaovitordantas <joaovitordantasss@hotmail.com>
2021-10-21 09:55:26 -0300

    eliminando comentarios arquivo fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -10,24 +10,3 @@ if (typeof module !== 'undefined') {
     fetchProducts,
   };
 }
-
-// const fetch = require('node-fetch');
-
-// const fetchProducts = async () => {
-//   const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-
-//   const products = await fetch(url)
-//     .then((response) => response.json())
-//     .then((results) => results)
-//     .catch((error) => error);
-
-//   return products;
-// };
-
-// console.log(fetchProducts);
-
-// window.onload = fetchProducts; // tem que ser chamada quando carrega a pagina
-// tem que ser chamada no script.js
-// acessar o OBJETO retornado por products e acessar o array que tem dentro apenas com o que o requisito pedir
-// data.results para acessar o array // so data para acessar o objeto
-// talvez um forEach para iterar com array

52b1a631ca2a20a5c8e4baa8f1cb8109e324ca4e
Laura <lahfumagalli@gmail.com>
2021-10-21 00:34:39 -0300

    requisito 2

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,10 +1,11 @@
 const fetchItem = async (id) => {
   // seu código aqui
+  if (!id) throw new Error('You must provide an url');
   const itemId = await fetch(`https://api.mercadolibre.com/items/${id}`);
   const itemIdJson = await itemId.json();
-  console.log(itemIdJson);
+  return itemIdJson;
 };
-fetchItem('MLB1341706310');
+
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchItem,
--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -40,13 +41,30 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-const funcFet = () => {
-  fetchProducts('computador').then((promisse) => promisse.forEach(({ id, title, thumbnail }) => {
-    const obj = { sku: id, name: title, image: thumbnail };
-    document.querySelector('.items').appendChild(createProductItemElement(obj));
-  }));
+const funcFet = (paramItem) => {
+  fetchProducts(paramItem)
+    .then((promisse) => promisse.forEach(({ id, title, thumbnail }) => {
+      const obj = { sku: id, name: title, image: thumbnail };
+      document.querySelector('.items').appendChild(createProductItemElement(obj));
+    }));
+};
+
+const btnAddCart = async (paramItem) => {
+  const product = await fetchProducts(paramItem);
+  const listOl = document.querySelector('ol');
+  document.querySelectorAll('.item__add').forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      const obj = {
+        sku: event.target.parentElement.firstChild.innerText,
+        name: event.target.previousSibling.previousSibling.innerText,
+      };
+      obj.salePrice = product.find(({ id }) => id === obj.sku).price;
+      listOl.appendChild(createCartItemElement(obj));
+    });
+  });
 };
 
 window.onload = () => {
-  funcFet();
+  funcFet('computador');
+  btnAddCart('computador');
 };

cc55d23f3abb237d097faf13dcca59690b425f60
aparecidag365 <mgegoulart365@gmail.com>
2021-10-21 00:44:00 -0300

    adicionando requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,9 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = (products) => {
+  return fetch (`https://api.mercadolibre.com/sites/MLB/search?q=${products}`)
+  .then((data) => data.json())
+  .catch((error) => error);
 };
+//fetchProducts();
 
 if (typeof module !== 'undefined') {
   module.exports = {

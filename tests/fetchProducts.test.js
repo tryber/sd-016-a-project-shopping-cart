2a875b11eae1c013f2ad957a00edca58bd5b70ea
Veidz <joaovictorveidz@gmail.com>
2021-10-20 21:33:54 -0300

    1 - ICriando a listagem de produtos, e implementando os testes

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,5 +6,22 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  it('Verifica se fetch foi chamada na função fetchProducts com argumento computador', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Verifica se o endpoint utilizado está correto', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+  it('Verifica se o retorno da função fetchProducts com o argumento computador é igual ao objeto computadorSearch', async () => {
+    const computers = await fetchProducts('computador');
+    expect(computers).toEqual(computadorSearch.results);
+  });
+  it('Verifica se ao chamar a função fetchProducts sem argumento, retorna uma mensagem de erro', () => {
+    expect(fetchProducts).toThrow(new Error('You must provide an url'));
+  });
 });

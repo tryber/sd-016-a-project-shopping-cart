8499096172ee3525077ce0088514b75f8bbf1066
mateusturola <turolamateus@gmail.com>
2021-10-20 22:15:11 -0300

    implementa testes

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,7 +2,7 @@ const fetchProducts = async (product) => {
   const urlAPI = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
   const response = await fetch(urlAPI);
   const data = await response.json();
-  return data.results;
+  return data;
 };
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -13,10 +13,6 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  // console.log(`sku: ${sku}
-  // name: ${name},
-  // image: ${image}
-  // ===========`);
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -28,34 +24,26 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
+} */
+/* 
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
-
-function createCartItemElement({ sku, name, salePrice }) {
+ */
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
 
 const loadProducts = async () => {
   const getItems = document.querySelector('.items');
   const array = await fetchProducts('computador');
-  array.forEach(({ id: sku, title: name, thumbnail: image }) => {
-    getItems.appendChild(createProductItemElement({ sku, name, image }));
-  }); 
-};
-
-const criarFrase = (array) => {
-  const getItems = document.querySelector('.items');
-    
-  array.forEach(({ id: sku, title: name, thumbnail:image }) => {
+  array.results.forEach(({ id: sku, title: name, thumbnail: image }) => {
     getItems.appendChild(createProductItemElement({ sku, name, image }));
   }); 
 };

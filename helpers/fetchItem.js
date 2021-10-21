a481538dd052a5038e363bb69dbb4cad21849675
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-10-20 21:07:32 -0300

    feat(fetchItemm): adicionou a funcao fetchItem conforme requisito 2

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
-  // seu código aqui
+const mercadoLibreItemUrl = (querry) => `https://api.mercadolibre.com/items/${querry}`;
+
+const fetchItem = async (querryUserInfo) => {
+  if (!querryUserInfo) {
+    throw new Error('You must provide an url');
+  }
+  const url = mercadoLibreItemUrl(querryUserInfo);
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {

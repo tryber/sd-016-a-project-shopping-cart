263d0021d26d295d4d5d9a4f021e3316fe802696
cdartora <cdartora>
2021-10-21 10:54:12 -0300

    feat(script): implementa eventlistener nos botões de adicionar ao carrinho

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const cartItem = document.querySelector('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -12,24 +14,12 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
+function cartItemClickListener(element, id) {
+  element.addEventListener('click', async () => {
+    const item = await fetchItem(id);
+    const { id: sku, title: name, price: salePrice } = item;
+    cartItem.appendChild(createCartItemElement({ sku, name, salePrice }));
+  });
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -38,6 +28,31 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function createCartButton(id) {
+  const b = document.createElement('button');
+  b.className = 'item__add';
+  b.innerText = 'Adicionar ao carrinho';
+  cartItemClickListener(b, id);
+
+  return b;
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCartButton(sku));
+
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 async function searchProducts(query) {
@@ -51,5 +66,5 @@ async function searchProducts(query) {
 }
 
 window.onload = () => { 
-  searchProducts('headset');
+  searchProducts('computador');
 };

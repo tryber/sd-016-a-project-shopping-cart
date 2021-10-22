be92167e417988e599b0fde9b748524a2a4a259c
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-10-22 01:28:03 -0300

    teste 1 saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,5 +8,9 @@ Object.defineProperty(window, 'localStorage', {
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('1- Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado.', () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
 });
+

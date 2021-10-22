27b0caeccb7944967aecfe3c1c5582769d09c6a9
joaogmelo95 <joaogmelo95@gmail.com>
2021-10-22 16:06:38 -0300

    Pequena mudança no arquivo de testes de getSavedCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -15,7 +15,7 @@ describe('4 - Teste a função saveCartItems', () => {
   it('Verifica se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
     const expectedStorageKey = 'cartItems';
     const expectedElement = '<ol><li>Item</li></ol>';
-    saveCartItems('<ol><li>Item</li></ol>');
+    saveCartItems(expectedElement);
     expect(localStorage.setItem).toHaveBeenCalledWith(expectedStorageKey, expectedElement);
   })
 });

10024bde9619a2d3b87d585d82277aef8e28d553
severo <oliveira.severo@gmail.com>
2021-10-22 18:01:01 -0300

    Requisito 01 [teste 3 - passou] - Verifica se, ao chamar fetchProducts com o argumento computador, a função utiliza o endpoint correto

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -9,5 +9,15 @@ describe('1 - Teste a função fecthProducts', () => {
   it ('É esperado que fetchProducts seja uma função', () => {
     expect(typeof fetchProducts).toBe('function')
   });
+
+  it ('Verifica se a função fetchProducts é chamada recebendo como parâmetro a palavra computador', async () => {
+    await fetchProducts('computador')
+    expect(fetch).toHaveBeenCalled();
+  });
+  it ('Verifica se, ao chamar fetchProducts com o argumento "computador", a função utiliza o endpoint correto "https://api.mercadolibre.com/sites/MLB/search?q=computador', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/sites/MLB/search?q=computador");
+  });
+
   // fail('Teste vazio');
 });

e3b426374781d0ba1c0db518f84d0d417c532018
MatheusBeniniF <matheus_benini@hotmail.com>
2021-10-22 19:14:44 -0300

    Testes do requisito 4

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const orderedList = document.querySelector('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
+}
+
+function cartItemClickListener(event) {
+  event.target.remove();
+  saveCartItems(orderedList.innerHTML);
 }
 
 function createCustomElement(element, className, innerText) {
@@ -12,15 +19,12 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function cartItemClickListener(event) {
-  event.target.remove();
-}
-
   function createCartItemElement({ sku, name, salePrice }) {
     const li = document.createElement('li');
     li.className = 'cart__item';
     li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
     li.addEventListener('click', cartItemClickListener);
+    saveCartItems(orderedList.innerHTML);
     return li;
   }
 
@@ -59,8 +63,21 @@ function listProducts(search) {
   createProductItemElement(product)));
 }
 
+const cartItemsRestora = () => {
+  const localStorageRestore = getSavedCartItems();
+  orderedList.innerHTML = localStorageRestore;
+};
+
+const restoreEventListener = () => {
+  Array.from(orderedList.children).forEach((child) => {
+    child.addEventListener('click', cartItemClickListener);
+  });
+};
+
 window.onload = () => { 
   listProducts('computador');
+  if (orderedList.children.length === 0) cartItemsRestore();
+  restoreEventListenerCartItem();
 };
 
 // Feito com auxilio de: Renan Souza, Lucas Alves, Fabricio Martins, Rafael Feliciano, Vitor Brandao em uma sala do zoom

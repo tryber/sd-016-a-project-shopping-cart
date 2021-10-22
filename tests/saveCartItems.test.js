4d9eaa8b4c6c39b27f6e3b44f1493b45fa7c4db6
Ronald Assis <ronaldassis71@gmail.com>
2021-10-21 23:12:46 -0300

    applying funcitons and test in the saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,24 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado.', (done) => {
+    try {
+      saveCartItems('cartItems', '<ol><li>Item</li></ol>');
+      expect(localStorage.setItem).toHaveBeenCalled();
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems.', (done) => {
+    try {
+      const value = '<ol><li>Item</li></ol>';
+      saveCartItems('cartItems',value);
+      expect(localStorage.setItem).toHaveBeenCalledWith('cartItems',value);
+      done()
+    } catch (err) {
+      done(err)
+    }
+  });
 });

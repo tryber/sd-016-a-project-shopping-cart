2f404946ec7ebd2804bf550633a39be2cbaa611f
Gregory <gregorysak90@gmail.com>
2021-10-22 17:43:17 -0300

    Requisito 1, feito(corrigindo lint)

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -3,11 +3,11 @@
 // e ela retornar algo em formato json.
 // 3- Caso a requisição da api falhe, usa o catch.
 // 4- A função fetch precisa ser retornada.
-const fetchProducts = (product) => {
+function fetchProducts(product) {
   return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
     .then((data) => data.json())
     .catch((error) => error);
-};
+}
 
 if (typeof module !== 'undefined') {
   module.exports = {

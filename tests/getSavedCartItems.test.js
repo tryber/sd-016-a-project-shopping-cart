55aaa194c355aa69b28206fd3154d8faa1e76ac7
PatrickR00t <patrick.s.falquetti@gmail.com>
2021-10-22 16:28:51 -0300

    .

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -7,11 +7,11 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  it ('A chamada de getSaveCartItems("<ol><li>Item</li></ol>" retorna o localStorage) ', () => {
+  it ('a chamada de getSaveCartItems("<ol><li>Item</li></ol>" retorna o localStorage) ', () => {
     getSavedCartItems('<ol><li>Item</li></ol>');
     expect(localStorage.getItem).toHaveBeenCalled();
   });
-  it ('Ao executar a função, o método localStorage.getItem é chamado com o parâmetro `cartItems`', () => {
+  it ('ao executar a função, o método localStorage.getItem é chamado com o parâmetro `cartItems`', () => {
     getSavedCartItems('cartItems');
     expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
   });

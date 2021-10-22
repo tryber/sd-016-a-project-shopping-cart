00eb4f7b9194e10a66e63e986e2d620f11ba97a2
Priscila Silva <priscilacds_100@yahoo.com.br>
2021-10-22 01:02:59 -0300

    Adiciona testes requisito 4

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,6 +1,4 @@
-const saveCartItems = () => {
-  // seu código aqui
-};
+const saveCartItems = (infos) => localStorage.setItem('cartItems', infos);
 
 if (typeof module !== 'undefined') {
   module.exports = saveCartItems;

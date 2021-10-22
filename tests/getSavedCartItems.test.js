6e0d116d157f61d38594339a9de86094d0856981
FelipeVahia <felipevahia@gmail.com>
2021-10-21 23:59:56 -0300

    4) Finalized

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -6,6 +6,15 @@ Object.defineProperty(window, 'localStorage', {
   },
 });
 
-describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
+describe('3 - Teste a função getSavedCartItems', () => {
+  it('Ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  })
+
+  it('Ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro', () => {
+    const expected = 'cartItems';
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith(expected);
+  })
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,5 +7,15 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
+  it('Ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  })
+
+  it('Ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    const expectedKey = 'cartItems';
+    const expectedValue = '<ol><li>Item</li></ol>';
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith(expectedKey, expectedValue);
+  })
 });

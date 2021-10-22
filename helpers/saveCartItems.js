78a9b513d13842d5173aec5a283c9237bf4b86eb
arctavares <cosilk@hotmail.com>
2021-10-22 14:33:18 -0300

    saveCartItems function

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,16 +1,16 @@
 const saveCartItems = () => {
   localStorage.clear()
-  const cart = document.querySelector('ol.cart__items').children;  // get cart ol
-  const cartListItems = [];
-  for (let i = 0; i < cart.length; i += 1) {
-    cartListItems.push(cart[i]);
-  }
-  const saveCart = {
-    'cartItems': cartListItems,
-  }
-  console.log(saveCart);
+  const cart = document.querySelector('ol.cart__items').children;  // get cart list
 
-  // CONTINUO AMANHA  
+  let idsString = '';
+  for(let i = 0; i < cart.length; i += 1){
+    for(let j = 4; j < 18; j += 1){
+      idsString += cart[i].innerText[j];
+    }
+    idsString += ',';
+  }
+  
+  localStorage.setItem('cartItems', idsString);
 };
 
 if (typeof module !== 'undefined') {

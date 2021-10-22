e202b5636dee9705e77d61da96229179c357b1f1
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-10-22 16:02:08 -0300

    fix: refatoracao do codigo para melhor independencia do codigo, transferindo linhas para o script.js

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,20 +1,6 @@
-// const saveCartItems = require('./saveCartItems');
-
-function cartItemClickListener(event) {
-  if (event.target.className === 'cart__item') {
-    event.target.remove();
-  }
-  // saveCartItems();
-  const cartList = document.querySelector('.cart__items');
-  const text = cartList.innerHTML;
-  localStorage.setItem('cartItems', JSON.stringify(text));
-}
-
 const getSavedCartItems = () => {
-  const getLocalStorage = JSON.parse(localStorage.getItem('cartItems'));
-  const cartItensElement = document.querySelector('.cart__items');
-  cartItensElement.innerHTML = getLocalStorage;
-  cartItensElement.addEventListener('click', cartItemClickListener);
+  const result = localStorage.getItem('cartItems');
+  return result;
 };
 
 if (typeof module !== 'undefined') {

5a5f11e103cffb31884dff2ef4e8def477b6caf1
brunão <btex88@gmail.com>
2021-10-21 23:44:41 -0300

    Requisito 04 - teste feito, mas o README tá uma... - complete a frase -

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,5 +8,12 @@ Object.defineProperty(window, 'localStorage', {
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifies if saveCartItem after being received the argument <ol><li>Item</li></ol>, the method localStorage.setItem is called', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Verifies if saveCartItem after being received the argument <ol><li>Item</li></ol>, the method localStorage.setItem is called with with first argument being "cartItems" and the second with the value passed as argument of saveCartItems', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });

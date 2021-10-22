c333ba2b4fb1dcff880092a995cb6dac973ccae3
Gregory <gregorysak90@gmail.com>
2021-10-22 17:05:45 -0300

    Requisito 1, feito

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+// 1- Buscando endpoint com o fetch de forma dinâmica.
+// 2- Espera o resultado da busca da api ficar pronto com o then
+// e ela retornar algo em formato json.
+// 3- Caso a requisição da api falhe, usa o catch.
+// 4- A função fetch precisa ser retornada.
+const fetchProducts = (product) => {
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+    .then((data) => data.json())
+    .catch((error) => error);
 };
 
 if (typeof module !== 'undefined') {

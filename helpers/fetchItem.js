290dbffefc39fcbd1aa7f3da4b6a578a695e6e65
rbfeliciano <rb.feliciano@hotmail.com>
2021-10-22 18:32:22 -0300

    Requisito 2 e 3, nao sei nem como consegui kkk

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,5 @@
 const fetchItem = async (id) => {
-    const url = fetch(`https://api.mercadolibre.com/items/$ItemID${id}`);
+    const url = fetch(`https://api.mercadolibre.com/items/${id}`);
     return url
     .then((data) => data.json())
     .catch((error) => error);
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const cartList = document.querySelector('.cart_items');
-const button = document.querySelector('.empty-cart');
-
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,41 +12,48 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
-const createProductItemElement = ({ sku, name, image }) => {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-};
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+function cartItemClickListener(event) {
+  event.target.remove();
 }
 
-const cartItemClickListener = (event) => {
-  event.target.remove();
-  itemsSave(cartList.innerHTML);
-};
-
-const createCartItemElement = ({ sku, name, salePrice }) => {
+const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+
   return li;
 };
 
-const addProductCart = async (sku) => {
-  const fetch = await fetchItem(sku);
-  console.log(fetch);
-  const { title: name, price: salePrice } = fetch;
-  createCartItemElement({ sku, name, salePrice });
+async function addToCart(id) {
+  const request = await fetchItem(id);
+  const getOl = document.querySelector('.cart__items');
+  const { id: sku, title: name, price: salePrice } = request;
+  const cartList = createCartItemElement({ sku, name, salePrice });
+  getOl.appendChild(cartList);
+}
+
+const createProductItemElement = ({ sku, name, image }) => {
+  const getSectionItems = document.querySelector('.items');
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const clickableButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho');
+  clickableButton.addEventListener('click', () => {
+    addToCart(sku);
+  });
+  section.appendChild(clickableButton);
+  getSectionItems.appendChild(section);
+  return section;
 };
+
+/* function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+ */
+
 /* Como eu fiz, alterado para como o BE fez.
 const fetchUrl = async () => {
   try {
@@ -78,13 +82,6 @@ const searchProducts = async (product) => {
     sectionItems.appendChild(productItem);
   });
 };
-
-const clearButton = () => {
-  cartList.innerHTML = '';
-  itemsSave(cartList.innerHTML);
-};
-
-button.addEventListener('click', clearButton);
 
 window.onload = () => {
   searchProducts('computador');

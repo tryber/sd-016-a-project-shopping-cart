3fa0a22f808a7c4a19f86b299045a76df0e6dbab
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2021-10-22 20:47:17 -0300

    Requisito 2 corrigido: Adicionar itens ao carrinho. E requisito 3 finalizado:  Remova o item do carrinho de compras ao clicar nele

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,6 +2,7 @@ const fetchItem = (itemID) => fetch(
   `https://api.mercadolibre.com/items/${itemID}`,
 )
 .then((response) => response.json())
+.then((data) => data)
 .catch((error) => error);
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -12,24 +12,8 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+ event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -39,36 +23,50 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-// Feito com o auxilio do vídeo gravado pelo Instrutor Bernardo Salgueiro.
-async function searchProducts(product) {
-const searchData = await fetchProducts(product);
-const sectionItems = document.querySelector('.items');
-searchData.results.forEach((item) => {
-  const itemObject = {
-    sku: item.id,
-    name: item.title,
-    image: item.thumbnail,
-  };
-  const productItem = createProductItemElement(itemObject);
-  sectionItems.appendChild(productItem);
-});
-}
 
 async function addItemsToCart(idProduct) {
   const searchData = await fetchItem(idProduct);
+  const { id: sku, title: name, price: salePrice } = searchData;
   const selectItem = document.querySelector('.cart__items');
-  searchData.results.forEach((selectedItem) => {
-    const selectedItemObj = {
-      sku: selectedItem.id,
-      name: selectedItem.title,
-      salePrice: selectedItem.price,
+  const getCartItems = createCartItemElement({ sku, name, salePrice });
+  selectItem.appendChild(getCartItems);
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const createButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  createButton.addEventListener('click', () => {
+    addItemsToCart(sku);
+  });
+  section.appendChild(createButton);
+
+  return section;
+}
+
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// Feito com o auxilio do vídeo gravado pelo Instrutor Bernardo Salgueiro.
+async function searchProducts(product) {
+  const searchData = await fetchProducts(product);
+  const sectionItems = document.querySelector('.items');
+  searchData.results.forEach((item) => {
+    const itemObject = {
+      sku: item.id,
+      name: item.title,
+      image: item.thumbnail,
     };
-    const addObjectProduct = createCartItemElement(selectedItemObj);
-    selectItem.appendChild(addObjectProduct);
+    const productItem = createProductItemElement(itemObject);
+    sectionItems.appendChild(productItem);
   });
 }
 
-window.onload = () => { 
- searchProducts('computador');
- addItemsToCart('MLB1615760527');
- };
+window.onload = () => {
+  searchProducts('computador');
+};

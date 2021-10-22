879480f9b2bfabd2b84d33472639b48b77139fe1
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-10-22 12:14:50 -0300

    Requisito 3 - remove item cart

--- script.js
+++ script.js
@@ -27,7 +27,9 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener() {
+function cartItemClickListener(event) {
+ const li = event.target;
+ li.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {

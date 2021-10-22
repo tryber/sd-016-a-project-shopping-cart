5fe1d1909b8b770d1313cd4b3ccabcbf833944a2
arctavares <cosilk@hotmail.com>
2021-10-21 21:59:15 -0300

    Test if fetch is called using parameter MLB1615760527

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,9 +4,16 @@ const item = require('../mocks/item');
 
 window.fetch = jest.fn(fetchSimulator);
 
+beforeEach(() => {
+  fetch.mockClear();
+})
+
 describe('2 - Teste a função fecthItem', () => {
   it('Test if fetchItem is a function', () => {
     expect(typeof(fetchItem)).toBe('function');
   })
-  
+  it('Test if fetch is called using parameter MLB1615760527', async () => {
+    await fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalledTimes(1);
+  })
 });

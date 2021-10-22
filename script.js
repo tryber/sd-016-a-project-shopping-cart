2e2690bb25a7decc34ff71f37535dca7ccda35ac
Ronald Assis <ronaldassis71@gmail.com>
2021-10-21 21:43:21 -0300

    Add product to cart by clicking 'add to cart' btn

--- script.js
+++ script.js
@@ -12,22 +12,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
@@ -40,6 +24,31 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+const addItemToCart = async (item) => {
+  const olItems = document.querySelector('.cart__items');
+  const data = await fetchItem(item);
+  const { id: sku, title: name, price: salePrice } = data;
+  const itemCart = createCartItemElement({ sku, name, salePrice });
+  olItems.appendChild(itemCart);
+};
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const buttonAdd = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  section.appendChild(buttonAdd);
+  buttonAdd.addEventListener('click', () => addItemToCart(sku));
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
 // help from teacher Bernardo!
 const serchProducts = async (product) => {
   const data = await fetchProducts(product);
@@ -50,6 +59,8 @@ const serchProducts = async (product) => {
     section.appendChild(productItem);
   });
 };
+
 window.onload = () => {
   serchProducts('computador');
- };
+  addItemToCart('MLB1615760527');
+};

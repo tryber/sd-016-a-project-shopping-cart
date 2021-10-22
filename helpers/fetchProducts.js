7264de945acfa6fe255fcad6b80d2ea786e03618
rbfeliciano <rb.feliciano@hotmail.com>
2021-10-22 15:13:28 -0300

    Arrumando lint

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,5 @@
 const fetchProducts = (product) => {
-  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
     .then((data) => data.json())
     .catch((error) => error);
 };

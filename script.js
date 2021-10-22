87990515c948fa48304a04a1a422f33975b0549f
Danilo Meneguela <meneguelaprog@hotmail.com>
2021-10-21 23:44:28 -0300

    Requisito 4 Completo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const ol = document.querySelector('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,20 +17,20 @@ function createCustomElement(element, className, innerText) {
 // ajuda do miyazaki na sala do zoom
 function cartItemClickListener(event) {
   event.target.remove();
+  saveCartItems(ol.innerHTML);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const ol = document.querySelector('.cart__items'); 
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   ol.appendChild(li);
+  saveCartItems(ol.innerHTML);
 }
 
 const getID = async (sku) => {
   const fetch = await fetchItem(sku);
-  console.log(fetch);
   const { title: name, price: salePrice } = fetch;
   createCartItemElement({ sku, name, salePrice });
 };
@@ -41,8 +43,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  const createBtn = section
-    .appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const createBtn = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
   createBtn.addEventListener('click', () => {
     getID(sku);
   });
@@ -62,6 +63,23 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+const restoreCart = () => {
+  const localRestore = getSavedCartItems();
+  ol.innerHTML = localRestore;
+};
+
+const resetRemoveClick = () => {
+  Array.from(ol.children).forEach((child) => {
+    child.addEventListener('click', cartItemClickListener);
+  });
+};
+
 window.onload = () => {
   loadProducts();
+  if (ol.children.length === 0) restoreCart();
+  resetRemoveClick();
 };
+
+// Array.from:
+// https://stackoverflow.com/questions/222841/most-efficient-way-to-convert-an-htmlcollection-to-an-array
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from

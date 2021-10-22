19332afcb65ad590d17d514ef3da282c9d26b565
arctavares <cosilk@hotmail.com>
2021-10-21 22:08:20 -0300

    Test if parameter is called in the function fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -8,12 +8,16 @@ beforeEach(() => {
   fetch.mockClear();
 })
 
-describe('2 - Teste a função fecthItem', () => {
+describe('2 - Teste a função fetchItem', () => {
   it('Test if fetchItem is a function', () => {
-    expect(typeof(fetchItem)).toBe('function');
+    expect(typeof (fetchItem)).toBe('function');
   })
   it('Test if fetch is called using parameter MLB1615760527', async () => {
     await fetchItem('MLB1615760527')
     expect(fetch).toHaveBeenCalledTimes(1);
+  })
+  it('Test if parameter is called in the function', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527')
   })
 });

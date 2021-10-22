20c460d623057873fd8c751b641e5e1701e30603
Lucas Alves <lucasdolohov@gmail.com>
2021-10-22 18:02:53 -0300

    requisito 2

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,9 @@
-const fetchItem = () => {
+const fetchItem = (itemId) => {
   // seu código aqui
+  const url = `https://api.mercadolibre.com/items/${itemId}`;
+  return fetch(url)
+  .then((response) => response.json())
+  .catch((err) => err);
 };
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -12,6 +12,24 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function addCartItem(id) {
+  const cartList = document.querySelector('.cart__items');
+  const product = await fetchItem(id);
+  cartList.appendChild(createCartItemElement(product));
+}
+
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -19,7 +37,11 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  button.addEventListener('click', () => {
+    addCartItem(sku);
+  });
+  section.appendChild(button);
   sectionItems.appendChild(section);
 
   return section;
@@ -29,23 +51,12 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 function listProducts(searchUser) {
   return fetchProducts(searchUser)
   .then((data) =>
-  data.results.forEach((product) =>
-  createProductItemElement(product)));
+  data.results.forEach((product) => {
+  createProductItemElement(product);
+  }));
   /* Requisito feito durante uma sala de estudos.
    Auxiliado por: Renan Souza, Matheus Benini, Vitor Brandão, Fabrício Martins e Rafael Feliciano */
 }

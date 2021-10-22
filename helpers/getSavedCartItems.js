d4e7c4144b3fd05a325a6ed2e4e869a5afd8c78d
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-10-21 21:43:00 -0400

    implement: requisito 4 completo com a funcionalidade de remover os itens carregados no localStorage

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,12 +1,6 @@
 const getSavedCartItems = () => {
-  const odinPaiDeTodos = document.querySelector('.cart__items');
-
-  const getKey = JSON.parse(localStorage.getItem('listCarsKeyNew'));
-  for (let i = 0; i < getKey.length; i += 1) {
-    const thorNew = document.createElement('li');
-    thorNew.innerText = getKey[i];
-    odinPaiDeTodos.appendChild(thorNew);
-  }
+  const getKey = (localStorage.getItem('cartItems'));
+  return getKey;
 };
 if (typeof module !== 'undefined') {
   module.exports = getSavedCartItems;
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const odinPaiDeTodos = document.querySelector('.cart__items');
-const olItens = document.querySelector('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,7 +26,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
-  olItens.appendChild(li);
+  odinPaiDeTodos.appendChild(li);
   // help do Brunão
   saveCartItems(odinPaiDeTodos.innerHTML);
 }
@@ -62,10 +62,25 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+const verifyFunctionVoidAndRestore = () => {
+  const localStorageGetSavedCartItem = getSavedCartItems();
+  odinPaiDeTodos.innerHTML = localStorageGetSavedCartItem;
+};
+
+const addEventToItemSaved = () => {
+  // referência: https://stackoverflow.com/questions/222841/most-efficient-way-to-convert-an-htmlcollection-to-an-array
+  Array.from(odinPaiDeTodos.children).forEach((element) => {
+    element.addEventListener('click', cartItemClickListener);
+  });
+};
+
 window.onload = () => {
+  // help do Brunão
+  if (odinPaiDeTodos.children.length === 0) verifyFunctionVoidAndRestore();
   const getProducts = fetchProducts('computador').then((value) => {
     value.results.forEach((element) => {
       createProductItemElement(element);
     });
   });
+  addEventToItemSaved();
 };

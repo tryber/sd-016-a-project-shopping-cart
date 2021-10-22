aaa256cda2ac1d6c58efa9729445380b8ca27d3b
aparecidag365 <mgegoulart365@gmail.com>
2021-10-22 15:55:50 -0300

    corrigindo erros Lint - requisito 2

--- script.js
+++ script.js
@@ -12,14 +12,29 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui ()
+}
+
+// eslint-disable-next-line no-unused-vars
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 async function itemProducts(itemsId) {
   const itemData = await fetchItem(itemsId);
   const sectionItem = document.querySelector('.cart__items');
   const { id: sku, title: name, price: salePrice } = itemData;
   const itemLi = createCartItemElement({ sku, name, salePrice });
-  // // const(itemId:sku )
   sectionItem.appendChild(itemLi);
-  // });
 }
 
 function createProductItemElement({ sku, name, image }) {
@@ -35,23 +50,6 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui ()
-}
-
-// eslint-disable-next-line no-unused-vars
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
 
 async function searchProducts(product) {
   const searchData = await fetchProducts(product);

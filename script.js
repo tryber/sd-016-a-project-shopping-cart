1059ba682f83e22b27a3e23594abc35a4b5f5e2f
Priscila Silva <priscilacds_100@yahoo.com.br>
2021-10-23 17:19:27 -0300

    Adiciona requisito 4

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const list = document.querySelector('.cart__items');
 
+const localStorageList = []; // Criei um array vazio para dar push toda vez que salvar uma lista de produtos no localStorage.
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,14 +17,22 @@ function createCustomElement(element, className, innerText) {
 }
 
 function cartItemClickListener(event) {
-  event.target.remove();
+  event.target.remove();  
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', (event) => {
+    cartItemClickListener(event);
+    localStorageList.forEach((elementObj, index) => {
+      if (elementObj.sku === sku) {
+        localStorageList.splice(index, 1);
+      }
+    });
+    saveCartItems(JSON.stringify(localStorageList)); // Aqui o forEach na minha lista do localStorage foi feito junto com o splice para poder apagar algum elemento (objeto com infos do produto) quando o evento do click de APAGAR algum item do carrinho acontecer.
+  });
   return li;
 }
 
@@ -40,7 +50,12 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  button.addEventListener('click', () => buildCarItem(sku));
+  button.addEventListener('click', () => { 
+    buildCarItem(sku);
+    const productObj = { sku, name, image }; // Criando o objeto de produtos nessa função também para poder dar o push no localStorageList toda vez que algum botão de um item for clicado. 
+    localStorageList.push(productObj);
+    saveCartItems(JSON.stringify(localStorageList)); // Chamando a função setItem do localStorage aqui porque é nessa função que a lista de produtos foi criada ao clicar no botão (addEventListener), o JSON.stringify vai deixar eu visualizar o conteúdo como objeto na área salva do localStorage (application).
+  });
   section.appendChild(button);
   return section;
 }
@@ -60,10 +75,16 @@ async function buildProductItem(product) {
   });
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+function getLocalStorageList() {
+  const localList = JSON.parse(getSavedCartItems('cartItems'));
+  localList.map((item) => buildCarItem(item.sku));
+  }
 
 window.onload = async () => {
   await buildProductItem('computador');
+  getLocalStorageList();
  };

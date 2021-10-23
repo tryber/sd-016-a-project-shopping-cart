0ba3a1c3e693b8047feeae3714cb1562cc7a8f53
Renan Souza <s.renan@outlook.com>
2021-10-22 21:41:31 -0300

    Implemeta testes unitarios para o metodo localStorage

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,5 +8,12 @@ Object.defineProperty(window, 'localStorage', {
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se localStorage.item é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>')
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('Testa os parâmetros chamados pelo método localStorage.setItem', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });

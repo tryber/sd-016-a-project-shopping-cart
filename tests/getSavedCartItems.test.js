c1f18cbfc04ede817ac579b968b830070809b7bd
Igor <iigorfelipe@gmail.com>
2021-10-23 15:20:29 -0300

    implementa testes em getSavedCartItems.test

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -12,5 +12,13 @@ Object.defineProperty(window, 'localStorage', {
 // Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o 'cartItems' como parâmetro.
 
 describe('4 - Teste a função getSavedCartItems', () => {
+  it('ao executar getSavedCartItems, o método localStorage.getItem é chamado',() => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  })
 
+  it('o executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro', () => {
+    getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  })
 });

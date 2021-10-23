c56f00a08624bf9b1244e222bac52defb28d3d99
Fabiana Lopes Santana <fabianalopes93@outlook.com>
2021-10-22 21:51:53 -0300

    4

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function createCustomElement(element, className, innerText) {
 }
 
 function cartItemClickListener(event) {
-  event.target.remove();
+  getOlList.removeChild(event.target);
   saveCartItems(getOlList.innerHTML);
 }
 

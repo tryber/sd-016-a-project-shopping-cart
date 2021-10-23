fd871ae1c7d51560019c7fd9dd7c35f0f8603653
João Vitor Spala <joaovitorspala.js@gmail.com>
2021-10-22 23:38:42 -0300

    TESTS: Concluidos dos testes da função fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,25 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Deve ser uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Ao ser chamada com o argumento "MLB1615760527", fetch deve ser chamada', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Ao ser chamada com o argumento "MLB1615760527", fetch deve utilizar o endpoint correto', () => {
+    const expectedEndpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(expectedEndpoint);
+  });
+  it('Ao ser chamada com o argumento "MLB1615760527", fetch deve retornar uma estrutura de dados igual à esperada', async () => {
+    const result = await fetchItem('MLB1615760527');
+    expect(result).toEqual(item);
+  });
+  it('Ao ser chamada com o argumento "MLB1615760527", fetch deve retornar uma estrutura de dados igual à esperada', async () => {
+    const expectedError = new Error('You must provide an url');
+    const result = await fetchItem();
+    expect(result).toEqual(expectedError);
+  });
 });

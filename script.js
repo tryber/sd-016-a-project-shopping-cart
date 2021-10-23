f3c8d33b04a68a0a35b2de313de4da3af05a3896
severo <oliveira.severo@gmail.com>
2021-10-23 18:13:47 -0300

    Testes do requisito 02 passaram

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// cria os cards dos produtos
 function createProductImageElement(imageSource) {
   const conteiner = document.createElement('div');
   conteiner.className = 'conteiner_img';
@@ -10,6 +11,12 @@ function createProductImageElement(imageSource) {
   return conteiner;
 }
 
+function cartItemClickListener(event) {
+  const li = event.target;
+  li.innerHTML = '';
+  li.remove();
+}
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -17,41 +24,38 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image, salePrice }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('span', 'priceProduct', `R$ ${salePrice}`));
-  // section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  const buttonItem = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-  buttonItem.addEventListener('click', () => itemProducts(sku));
-  section.appendChild(buttonItem);
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-async function cartItemClickListener(event) {
-// vai ser para remover o item
-}
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: ${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
+async function addProductToCart(productID) {
+  const itemData = await fetchItem(productID);
+  const sectionItem = document.querySelector('.cart__items');
+  const { id: sku, title: name, price: salePrice } = itemData;
+  const chartItem = createCartItemElement({ sku, name, salePrice });
+  sectionItem.appendChild(chartItem);
+}
+
+function createProductItemElement({ sku, name, image, salePrice }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'priceProduct', `R$ ${salePrice}`));
+  const buttonItem = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  buttonItem.addEventListener('click', () => addProductToCart(sku));
+  section.appendChild(buttonItem);
+  return section;
+}
+
 async function serchProducts(product) { // essa e uma funcao assincrona
   const searchData = await fetchProducts(product); // chamada da funcao fetchproduts.js
-  const sectionItems = document.querySelector('.items'); // Alvo é a classe items no html
+  const sectionItems = document.querySelector('.items'); // Target HTMl
   searchData.results.forEach((item) => { // results é palcançar os dados no array
     const itemObject = {
       sku: item.id,
@@ -64,6 +68,10 @@ async function serchProducts(product) { // essa e uma funcao assincrona
   });
 }
 
+// function getSkuFromProductItem(item) {
+//   // return item.querySelector('span.item__sku').innerText;
+// }
+  
 window.onload = () => { 
   serchProducts('computador');
 };
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,29 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it ('É esperado que fetchItem seja uma função', () => {
+    expect(typeof fetchItem).toBe('function')
+  });
+
+  it ('Verifica se a função fetchItem é chamada quando o parametro MLB1615760527 é passado', async () => {
+    await fetchItem('MLB1615760527')
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it ('Verifica se, chamar a função fetchItem com o argumento do item "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith("https://api.mercadolibre.com/items/MLB1615760527");
+  });
+
+  it ('Verifica se o retorno da função fetchItem com o argumento "MLB1615760527" é um objeto igual à computadorSearch', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+
+  it('Verifica se ao chamar a função fetchItem sem argumentos retorna uma mensagem de erro', async () => {
+    const expectedError = new Error('You must provide an url');
+    const result = await fetchItem();
+    expect(result).toEqual(expectedError);
+  });
+
 });

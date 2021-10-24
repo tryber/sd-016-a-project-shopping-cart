3a2c87e7ac64395031089cf4ac6464fc94ccfd9e
arctavares <cosilk@hotmail.com>
2021-10-24 17:19:30 -0300

    fetchProducts refatored

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,14 +1,7 @@
-const fetchProducts = async (search) => {
-  if (search === undefined) {
-    throw new Error('You must provide an url');
-  }
-
-  const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
+const fetchProducts = async (search) =>
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
     .then((data) => data.json())
     .catch((error) => error);
-
-  return result;
-};
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,12 +4,7 @@ const computadorSearch = require('../mocks/search');
 
 window.fetch = jest.fn(fetchSimulator);
 
-beforeEach(() => {
-  fetch.mockClear();
-});
-
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
   const parameterPC = 'computador';
   it('Test if fetchProducts is a function', () => {
     expect(typeof (fetchProducts)).toBe('function');
@@ -22,19 +17,12 @@ describe('1 - Teste a função fecthProducts', () => {
     fetchProducts(parameterPC);
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   })
-
   it('Test if fetchProducts(computador) equals to computadorSearch', async () => {
     const products = await fetchProducts(parameterPC);
     expect(products).toBe(computadorSearch);
   })
-
   it('Test if fetchProducts() equals to error(You must provide an url)', async () => {
-    try {
-      await fetchProducts();
-    } catch (e) {
-      expect(e).toEqual(new Error('You must provide an url'));
-    }
-
+    const callFetchProducts = await fetchProducts();
+    expect(callFetchProducts).toEqual(new Error('You must provide an url'));
   })
-
 });

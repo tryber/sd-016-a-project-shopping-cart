abf263da631798b4add4b8f94974679bc1c4b416
FernandoMos92 <fernandomosp@gmail.com>
2021-10-24 12:45:14 -0300

    repair lint problem with arrow function

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,8 +1,12 @@
 // endpoint: "https://api.mercadolibre.com/sites/MLB/search?q=$QUERY"
 
-const fetchProducts = (product) => fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
+const fetchProducts = (product) => {
+  const result = fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
   .then((data) => data.json())
-  .catch((error) => error);
+.catch((error) => error);
+
+return result;
+};
 
 if (typeof module !== 'undefined') {
   module.exports = {

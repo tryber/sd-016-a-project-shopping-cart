b86860f9c79deda968e6a68b86861347761c6775
FernandoMos92 <fernandomosp@gmail.com>
2021-10-25 11:47:39 -0300

    Third test for fetchItem completed

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,12 +5,18 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  it('Testa se fetchItem e uma funcao', () => {
+  it('Testa se fetchItem e uma funcao.', () => {
     expect(typeof fetchItem).toBe('function')
   })
 
-  it('Testa se a funcao fetch foi chamada', () => {
+  it('Testa se a funcao fetch foi chamada.', () => {
     fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenCalled(); 
+  })
+
+  it('Verifica se chamada com o item MLB1615760527 o endPoint corresponde.', () => {
+    const endpoint = "https://api.mercadolibre.com/items/MLB1615760527";
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
   })
 });

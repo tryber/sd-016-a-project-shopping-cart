b743649597f98febe52a829acf660d1d61b1a62d
obastosleandro <bastosleandrodesigner@gmail.com>
2021-10-25 16:54:31 -0300

    sobe a questao 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,9 @@
-const fetchProducts = () => {
-  // seu código aqui ETC E TAL ETC ETC ETC
+const fetchProducts = (product) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  return fetch(url)
+  .then((data) => data.json())
+  .then((response) => response.results)
+  .catch((error) => error);
 };
 
 if (typeof module !== 'undefined') {

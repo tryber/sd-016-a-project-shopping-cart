b48be806620da3f6bd22b98d0786edd7427f36cb
Otavio Cantareli <otavio_cantareli@outlook.com>
2021-10-25 19:55:19 -0300

    Teste

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,7 +7,7 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  it('method is called', () => {
-    
-  });
+  // it('method is called', () => {
+
+  // });
 });

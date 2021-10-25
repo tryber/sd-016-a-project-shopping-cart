c474dca960ef2cb77adeb0ef31cfd632ecd817bf
João Vitor Spala <joaovitorspala.js@gmail.com>
2021-10-25 20:18:03 -0300

    TESTS: Concluidos os testes para a função saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,14 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  describe('Ao ser executada com o argumento <ol><li>Item</li></ol>: ', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    it('O método localStorage.setItem é chamado.', () => {
+      expect(localStorage.setItem).toHaveBeenCalled();
+    });
+    it('O método localStorage.setItem é chamado com os argumentos corretos.', () => {
+      expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+    });
+  });
+  
 });

b688090ba294a0f50a280c996662875f3b5e4d87
Marcus <marcus10121@hotmail.com>
2021-10-25 16:40:38 -0300

    Adiciona o Requisito 02

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function cartItemClickListener(event) {
+// function cartItemClickListener(event) {
  
-}
+// }
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
 }
 

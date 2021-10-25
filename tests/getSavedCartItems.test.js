bc79e52a63533d4dc93352f240bdba1e41b34956
rbfeliciano <rb.feliciano@hotmail.com>
2021-10-25 16:21:39 -0300

    Finaizando o teste getSavedCartItems

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -11,8 +11,8 @@ describe('4 - Teste a função getSavedCartItems', () => {
     getSavedCartItems();
     expect(localStorage.getItem).toHaveBeenCalled();
   });
-  it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o 'cartItems' como parâmetro', () => {
-    getSavedCartItems();
+  it('Teste se, ao executar getSavedCartItems o método localStorage.getItem é chamado', () => {
+    getSavedCartItems()
     expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
   });
 });

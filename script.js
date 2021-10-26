c3b40e463c52de5a6686a2b85ef8331459ca724b
Jonas Candia <jonas57.jc@gmail.com>
2021-10-26 18:27:14 -0300

    requisito 1

--- script.js
+++ script.js
@@ -40,4 +40,20 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+async function searchProducts(product) {
+  const searchData = await fetchProducts(product);
+  const sectionItens = document.querySelector('.items');
+  searchData.results.forEach((item) => {
+    const itemObject = {
+      sku: item.id,
+      name: item.title,
+      image: item.thumbnail,
+    };
+    const productItem = createProductItemElement(itemObject);
+    sectionItens.appendChild(productItem);
+  });
+}
+
+window.onload = () => {
+  searchProducts('computador');
+};

56acc10dd78cad7587a6776fb7c0d6b1cb9f0e9e
Thiago Ximenes <thiagoximeneslima@gmail.com>
2021-10-25 21:34:42 -0300

    ✅ test feat: Adiciona teste para a função getSavedCartItem e termina requisito 4.

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -11,5 +11,9 @@ describe('4 - Teste a função getSavedCartItems', () => {
   it('Testa se, ao executar getSavedCartItems, o método localStorage.getItem é chamado;', () => {
     getSavedCartItems()
     expect(localStorage.getItem).toHaveBeenCalled()
+  });
+  it('Testa se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro.', () => {
+    getSavedCartItems('cartItems')
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems')
   })
 });

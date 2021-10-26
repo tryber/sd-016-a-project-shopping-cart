d596dae71241939749706d65e6da9ee6279e64af
Angelica <angelicapedroso05@gmail.com>
2021-10-26 11:08:26 -0300

    testes saveCArtItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,17 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  
+  it('verifica se é uma função', () => {
+    expect(typeof saveCartItems).toBe('function');
+  })
+});
+
+it('Verifica se ao executar saveCartItems com o argumento, o método localStorage.setItem é chamado', () => {
+  saveCartItems('<ol><li>Item</li></ol>');
+  expect(localStorage.setItem).toHaveBeenCalled();
+});
+
+it('Verifica se ao executar saveCartItems com o argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro cartItems e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+  saveCartItems('<ol><li>Item</li></ol>');
+  expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
 });

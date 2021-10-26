8c5bad7ad8d93f73ebd86b48b42ccb62fca4a4c5
mabiane <mabiane.polniak@gmail.com>
2021-10-26 18:24:14 -0300

    [Requisito 5] Funçao Total Price implementada

--- script.js
+++ script.js
@@ -3,10 +3,33 @@
 
 const localOl = document.querySelector('ol'); // definimos o lugar da ol
 const localButtonEmptyCart = document.querySelector('.empty-cart');
+const localTotalPrice = document.querySelector('.total-price');
+
+async function totalPrice() {
+  let sumPrice = 0;
+
+  const fullListItems = await getSavedCartItems(); // retorna itens como string
+  const corte = fullListItems.split('$'); // separa em novos itens de um array a partir do 'R$'
+  corte.shift(); // remove o item zero, que contem a string anterior anterior ao primeiro corte
+
+  corte.forEach((item) => { // percorremos array, cada item se inicia com o preço 
+    sumPrice += parseFloat(item.substring(0, item.indexOf('<')));
+  });
+
+  /**
+    indexOf defini a posição do final da string para retornar somente numeros | sugestão do Brunão | https://www.w3schools.com/jsref/jsref_indexof.asp
+
+    substring é um método de retornar somente partes de uma string | sugestão do Brunão | https://www.w3schools.com/jsref/jsref_substring.asp | https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+
+    parseFloat converte os numeros retirados da string em numeros, podendo ser decimais | https://www.w3schools.com/jsref/jsref_parsefloat.asp
+  */
+  localTotalPrice.innerText = sumPrice;
+}
 
 function cartItemClickListener(event) { 
   event.target.remove(); // retira do carrinho o item clicado
   saveCartItems(localOl.innerHTML);
+  totalPrice();
 }
 
 function createCartItemElement({ sku, name, salePrice }) { // adiciona itens no carrinho
@@ -21,7 +44,9 @@ async function searchItem(sku) { // procura um item especifico
   const objectIdItem = await fetchItem(sku);
   const { title: name, price: salePrice } = objectIdItem; // desestruturamos os itens necessarios
   localOl.appendChild(createCartItemElement({ sku, name, salePrice })); // criamos o item no carrinho na ol
+
   saveCartItems(localOl.innerHTML);
+  totalPrice();
 }
 
 function createProductImageElement(imageSource) { // gera a imagem da vitrine
@@ -45,8 +70,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image, pric
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
-
-  // section.appendChild(createCartItemElement('span','price__item', salePrice));
+  section.appendChild(createCustomElement('span', 'price_item', `R$${salePrice} à vista`));
   
   const botao = (createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   botao.addEventListener('click', () => { // adiciona escutador que busca o item e joga pro carrinho
@@ -82,6 +106,7 @@ function reloadCart() {
 function emptyCart() {
   localOl.innerHTML = '';
   saveCartItems(localOl.innerHTML);
+  totalPrice();
 }
 
 localButtonEmptyCart.addEventListener('click', emptyCart);
@@ -89,4 +114,5 @@ localButtonEmptyCart.addEventListener('click', emptyCart);
 window.onload = () => {
   searchProducts('computador');
   reloadCart();
+  totalPrice();
 };

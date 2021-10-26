0b4c983350223674b739c48b86bc78797e803621
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-10-26 16:47:38 -0300

    Implementacao de alguns tests no requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,13 @@
-const fetchProducts = () => {
+const fetchProducts = (produto) => {
   // seu código aqui
-};
+
+  const promise = fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produto}`);
+  
+  promise.then((response) => response.json());
+  promise.then((data) => data);
+  
+  return promise;
+}
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,5 +6,18 @@ window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it ('Teste se fetchProduts é uma função: ', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it ('Executa a função fetchProducts com o argumento "computador" e teste se fetch', () => {
+    fetchProducts('computador');
+    // Dica sumo deu mentoria conseguir esse metodo para testar o fetch.
+    expect(fetch).toBeCalled();
+  });
+
+  it ('Ao chamar a função fetchProducts com o argumento "computador", a função retorna o endpoint', () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toBeCalledWith(endpoint);
+  });
 });

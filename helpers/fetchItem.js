c11fa7d483d61868b75d5766a27cfa97f2f232c4
Adson-Gomes-Oliveira <adsongoliveira2021@gmail.com>
2021-10-27 14:44:25 -0300

    Segundo Requisito concluido.

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
+const fetchItem = async (sku) => {
   // seu código aqui
+  try { 
+    const binaryRes = await fetch(`https://api.mercadolibre.com/items/${sku}`);
+    const transformJSON = await binaryRes.json();
+    return transformJSON;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {

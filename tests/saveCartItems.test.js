fd09aa117ed1b780f26bb4758a628ff0c3f3b6e9
Tiago Abranges <tiagoabranges@gmail.com>
2021-10-27 20:54:14 -0300

    tentativa testes

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,13 +7,12 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  it('O método localStorage.getItem deve ser chamado ao executar a função!', () => {
-    getSavedCartItems();
-    expect(localStorage.getItem).toHaveBeenCalled();
-  });
-
-  it('Ao executar a função, localStorage.getItem deve conter o parâmetro correto!', () => {
-    expect(getSavedCartItems())
-    .toEqual(localStorage.getItem('cartItems'));
-  });
+  it('Testa se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  })
+  it('Testa se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  })
 });

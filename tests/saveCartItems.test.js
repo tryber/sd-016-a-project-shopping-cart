e51212bf1bcdc0ea295b519fe6edfda7b087648b
furquin <laert.ff@gmail.com>
2021-10-28 14:23:37 -0300

    implementa testes para funçoes saveCartItems e getSavedCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,6 +7,13 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('verifica se ao chama saveCartItems com parâmetro -<ol><li>Item</li></ol>- localStorage.setItem é chamado', ()  => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+ 
+ it('verifica se ao chama saveCartItems com parâmetro -<ol><li>Item</li></ol>- localStorage.setItem é chamado com 2 parâmetros -cartItems- e o segundo com valor igual ao parâmetro inserido', () => {
+   saveCartItems('<ol><li>Item</li></ol>');
+   expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');
+ });
 });

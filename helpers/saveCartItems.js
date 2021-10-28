45c03f518e65c1c6142b2adc647359b28c71e997
furquin <laert.ff@gmail.com>
2021-10-28 12:03:26 -0300

    implementa funçoes getSavedCartItems e saveCartItems, implementa arquivo script.js

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,7 +1,5 @@
-const saveCartItems = () => {
-  // seu código aqui
-};
+const saveCartItems = (item) => localStorage.setItem("cartItems", item);
 
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = saveCartItems;
 }
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const cartList = document.querySelector(".cart__items");
+
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
+  const img = document.createElement("img");
+  img.className = "item__image";
   img.src = imageSource;
   return img;
 }
@@ -12,14 +14,15 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 function cartItemClickListener(event) {
-  return event.target.remove();
+  event.target.remove();
+  saveCartItems(cartList.innerHTML);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
+  const li = document.createElement("li");
+  li.className = "cart__item";
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener("click", cartItemClickListener);
   return li;
 }
 
@@ -28,53 +31,74 @@ async function itemToBuy(id) {
   const itemsId = {
     sku: cartItems.id,
     name: cartItems.title,
-    salePrice: cartItems.price,     
+    salePrice: cartItems.price,
   };
-  const cartList = document.querySelector('.cart__items');
+
   cartList.appendChild(createCartItemElement(itemsId));
+  saveCartItems(cartList.innerHTML);
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+  const section = document.createElement("section");
+  section.className = "item";
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement("span", "item__sku", sku));
+  section.appendChild(createCustomElement("span", "item__title", name));
   section.appendChild(createProductImageElement(image));
 
   /**
- * Consultei o repositório da Cris Souza para resolver essa parte.
- * Link- https://github.com/tryber/sd-016-a-project-shopping-cart/pull/103/commits/f38badebf20f5af0f8f3594981dbe9f5d7e02cd7
- */
-const addCartButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
-addCartButton.addEventListener('click', () => itemToBuy(sku));
-section.appendChild(addCartButton);
+   * Consultei o repositório da Cris Souza para resolver essa parte.
+   * Link- https://github.com/tryber/sd-016-a-project-shopping-cart/pull/103/commits/f38badebf20f5af0f8f3594981dbe9f5d7e02cd7
+   */
+  const addCartButton = createCustomElement(
+    "button",
+    "item__add",
+    "Adicionar ao carrinho!"
+  );
+  addCartButton.addEventListener("click", () => itemToBuy(sku));
+  section.appendChild(addCartButton);
   return section;
 }
 
 function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+  return item.querySelector("span.item__sku").innerText;
 }
 
 /**
  * Consultei o vídeo gravado pelo Bernardo Salgueiro para resolver essa parte.
  * Link- https://app.slack.com/client/TMDDFEPFU/C02A8CKT31U/thread/C02A8CKT31U-1634781501.006900
-* */
- 
+ * */
+
 async function searchProducts(product) {
   const searchData = await fetchProducts(product);
-  const sectionItems = document.querySelector('.items');
+  const sectionItems = document.querySelector(".items");
   searchData.results.forEach((item) => {
- const itemCatalog = {
-   sku: item.id,
-   name: item.title,
-   image: item.thumbnail,
- };
- const section = createProductItemElement(itemCatalog);
- sectionItems.appendChild(section);
+    const itemCatalog = {
+      sku: item.id,
+      name: item.title,
+      image: item.thumbnail,
+    };
+    const section = createProductItemElement(itemCatalog);
+    sectionItems.appendChild(section);
   });
 }
 
-window.onload = () => { 
-  searchProducts('computador');
+/**
+ * Consultei o repositório da Cris Souza para resolver essa parte.
+ * Link- https://github.com/tryber/sd-016-a-project-shopping-cart/pull/103/commits/f38badebf20f5af0f8f3594981dbe9f5d7e02cd7
+ */
+
+const refreshPage = () => {
+  const getResult = getSavedCartItems();
+  cartList.innerHTML = getResult;
+};
+
+const restoreEventListener = () => {
+  cartList.addEventListener("click", cartItemClickListener);
+};
+
+window.onload = () => {
+  searchProducts("computador");
+  refreshPage();
+  restoreEventListener();
 };

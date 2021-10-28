5a5b350fcf3eab2c63419365c2a4d6c6550eeefe
Gabriel Alves <2kgabrielalves@gmail.com>
2021-10-28 11:53:48 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -40,4 +40,21 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+async function searchProduct(product) {
+  const searchResponse = await fetchProducts(product);
+  const sectionItems = document.querySelector('.items');
+  searchResponse.results.forEach((item) => {
+    const itemObject = {
+    sku: item.id,
+    name: item.title,
+    image: item.thumbnail,
+    };
+
+    const productItem = createProductItemElement(itemObject);
+    sectionItems.appendChild(productItem);
+  });
+}
+
+window.onload = () => {
+  searchProduct('computador');
+};

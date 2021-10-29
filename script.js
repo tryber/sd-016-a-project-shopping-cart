23891930156e19da1af3da1c0ad27121e1b86f09
LHenriquerios <henriquerios4@gmail.com>
2021-10-29 13:55:54 -0300

    Requisito 2 e e correção do primeiro

--- script.js
+++ script.js
@@ -12,24 +12,8 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  // adicione seu código aqui
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -37,7 +21,35 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  
   return li;
+}
+
+async function addItemToCart(item) {
+  const dataCart = await fetchItem(item);
+  const sectionItemsToCart = document.querySelector('.cart__items');
+  sectionItemsToCart.appendChild(createCartItemElement({
+    sku: dataCart.id,
+    name: dataCart.title,
+    salePrice: dataCart.price,
+}));
+  }
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  section.appendChild(button);
+  button.addEventListener('click', () => addItemToCart(sku));
+
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 async function searchProducts(product) {
@@ -49,13 +61,12 @@ async function searchProducts(product) {
       name: element.title,
       image: element.thumbnail,
     };
-    // const { sku: id, title: name, thumbnail: image } = element;
-    const productItem = createCartItemElement(itemObject);
+    // const { id: sku, title: name, thumbnail: image } = element;
+    const productItem = createProductItemElement(itemObject);
     sectionItems.appendChild(productItem);
   });
 }
 
 window.onload = () => {
-  console.log(searchProducts('computador'));
-  console.log(fetchItem('MLB1341706310'));
+  searchProducts('computador');
  };
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,10 +5,30 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  const sum = (a, b) => a + b;
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
-});
-  fail('Teste vazio');
+  it ('Deve ser uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+
+  it ('Ao chamá-la com o argumento "MLB1615760527", testa se Fetch foi chamada ', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it ('Ao chama-lá com o argumento "MLB1615760527", testa se fetch foi chamada com o Endpoint correto', () => {
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527'
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
+  });
+
+  it('Testa se o retorno da função é um objeto igual a item', async () => {
+    const results = await fetchItem('MLB1615760527');
+    expect(results).toEqual(item);
+  });
+
+  it ('Deve retornar um erro', async () => {
+    const expectError = new Error('You must provide an url');
+    const result = await fetchItem();
+    expect(result).toEqual(expectError);
+  });
 });

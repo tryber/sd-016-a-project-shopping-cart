93c2e9f63386091417792875c32e74939df95717
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-10-31 15:37:45 -0300

    Requisito 2: Cobre 100% dos testes

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -5,6 +5,25 @@ const item = require('../mocks/item');
 window.fetch = jest.fn(fetchSimulator);
 
 describe('2 - Teste a função fecthItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('deve ser uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('ao executá-la com o argumento MLB1615760527, testa se o fetch foi chamado', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('ao executá-la com o argumento MLB1615760527, testa se o fetch usa o endpoint correto', () => {
+    const endpoint = "https://api.mercadolibre.com/items/MLB1615760527";
+    fetchItem('MLB1615760527');
+    expect(fetch).toBeCalledWith(endpoint);
+  });
+  it('ao executá-la com o argumento MLB1615760527, deve retornar um objeto igual a item', async () => {
+    const results = await fetchItem('MLB1615760527');
+    expect(results).toEqual(item);
+  });
+  it('deve retornar um erro', async () => {
+    const expectedError = new Error('You must provide an url');
+    const result = await fetchItem();
+    expect(result).toEqual(expectedError);
+  });
 });

b90360b54e8aa21bf1b4a5293806ec1c256ad6b1
Edgar Amaral <edgar@Eddiexx86.com>
2021-10-31 19:55:48 -0300

    refatora a funçao fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,13 +1,9 @@
-const fetchItem = async (ItemID) => {
-  try {
-    const response = await fetch(
-      `https://api.mercadolibre.com/items/${ItemID}`,
-    );
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    return error;
-  }
+const fetchItem = async (id) => {
+  // seu código aqui
+  if (!id) throw new Error('You must provide an url');
+  const itemId = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const itemIdJson = await itemId.json();
+  return itemIdJson;
 };
 
 if (typeof module !== 'undefined') {

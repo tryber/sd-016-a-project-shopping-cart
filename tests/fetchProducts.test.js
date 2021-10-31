b386a61619c911d88415c4d79cc6fc8df0110363
Claudio Junior Haas <claudiohaas.cjh@gmail.com>
2021-10-31 15:16:10 -0300

    Retatoraçao testes requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -8,20 +8,20 @@ describe('1 - Teste a função fecthProducts', () => {
   it('deve ser uma função', () => {
     expect(typeof fetchProducts).toBe('function');
   })
-  it('ao executá-la com o parâmetro computador, testa se o fetch foi retornado', () => {
+  it('ao chamá-la com o argumento computador, testa se o fetch foi chamado', () => {
     fetchProducts('computador');
-    expect(fetch).toBeCalled();
+    expect(fetch).toHaveBeenCalled();
   });
-  it('ao executá-la com o parâmetro computador, testa se o fetch retorna o endpoint correto', () => {
+  it('ao chamá-la com o argumento computador, testa se o fetch retorna o endpoint correto', () => {
     const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
     fetchProducts('computador');
     expect(fetch).toBeCalledWith(endpoint);
   });
-  it('testa se o retorno da função é um objeto igual a computadorSearch', async () => {
+  it('o retorno da função deve ser um objeto igual a computadorSearch', async () => {
     const results = await fetchProducts('computador');
     expect(results).toEqual(computadorSearch);
   });
-  it('caso fetchProduct for chamada sem argumento, retorne um erro', async () => {
+  it('deve retornar um erro', async () => {
     const expectedError = new Error('You must provide an url');
     result = await fetchProducts();
     expect(result).toEqual(expectedError);

7089022abbe573fb08d5c58f3bc7af54f1678f34
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-11-01 02:08:00 -0300

    add requisito 10

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -8,5 +8,18 @@ Object.defineProperty(window, 'localStorage', {
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  // Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado;
+  it('if saveCartItems is called with <ol><li>Item</li></ol>, localStorage.setItem, must be called.', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  // Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro 'cartItems' e o segundo sendo o valor passado como argumento para saveCartItems.
+  it('message', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      'cartItems',
+      '<ol><li>Item</li></ol>',
+    );
+  });
 });

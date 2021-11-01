de624b789f950ba1072d6e736f7217c275054aff
Edgar Amaral <edgar@Eddiexx86.com>
2021-10-31 21:40:12 -0300

    Refatora o requisito #2

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', (event) => cartItemClickListener(event, sku));
+  listOl.appendChild(li);
+  return li;
+}
+
+const productById = async (idProduct) => {
+  const objProduct = await fetchItem(idProduct);
+  const { id: sku, title: name, price: salePrice } = objProduct;
+  const objParam = { sku, name, salePrice };
+  listOl.appendChild(createCartItemElement(objParam));
+  shopCartSave.push(objParam);
+  sumPrice(shopCartSave);
+  saveCartItems(JSON.stringify(shopCartSave));
+};
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -39,14 +58,6 @@ const productByName = async (paramItem) => {
 
 // function getSkuFromProductItem(item) {
 //  return item.querySelector('span.item__sku').innerText;
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//  const li = document.createElement('li');
-//  li.className = 'cart__item';
-//  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//  li.addEventListener('click', cartItemClickListener);
-//  return li;
 // }
 
 // function createCartItemElement({ sku, name, salePrice }) {

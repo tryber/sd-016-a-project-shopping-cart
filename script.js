c80773eed9b1c9c96616ec07a21e3f4eede60a55
Edgar Amaral <edgar@Eddiexx86.com>
2021-10-31 22:10:02 -0300

    Implementa o requisito #3

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Com colaboração de Priscila Silva
+
+const shopCartSave = [];
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';

74281d0367119786ea4ac514806dccab2bd3af36
Leonardo_Nunes <leonardokarate95@hotmail.com>
2021-11-02 15:35:20 -0300

    fix lint

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,9 +2,9 @@ const fetchItem = (id) => {
   if (!id) {
     throw new Error('You must provide an url');
   }
-  const promessa = fetch(`https://api.mercadolibre.com/items/${id}`)
-  const promessaJson = promessa.then((promessa) => promessa.json())
-  return promessaJson;
+  const resultado = fetch(`https://api.mercadolibre.com/items/${id}`)
+  .then((promessa) => promessa.json());
+  return resultado;
 };
 
 if (typeof module !== 'undefined') {

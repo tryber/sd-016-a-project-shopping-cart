8c0c1d770a9e30294bd956af9d54b90fef856cc9
Gustavo Ellwanger <ellwangerinvestimentos@gmail.com>
2021-11-02 15:47:06 -0300

    Adiciona fetchItem ao projeto

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,7 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = (cartProduct) => {
+  fetch(`https://api.mercadolibre.com/items/${cartProduct}`)
+    .then((data) => data.json())
+      .catch((error) => error);
 };
 
 if (typeof module !== 'undefined') {

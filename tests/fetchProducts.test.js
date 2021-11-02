798561e208913191b20310b12121e84a13f97d0e
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-11-02 18:53:23 -0300

    Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url. Dica: Lembre-se de usar o new Error('mensagem esperada aqui') para comparar com o objeto retornado da API

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -22,13 +22,14 @@ describe('1 - Teste a função fecthProducts', () => {
     expect(fetch).toHaveBeenCalledWith(endpoint);
   });
 
-  it('testa se o retrno da função se é um objeto igual a computadorSearch', async () => {
+  it('testa se o retorno da função se é um objeto igual a computadorSearch', async () => {
     const results = await fetchProducts('computador');
     expect(results).toEqual(computadorSearch);
   });
-  // it('deve retornar um erro', () => {
-  //   const expectError = new Error('you must provide an url');
-  //   const results = await fetchProducts();
-  //   expect(result).toEqual(expectError);
-  // });
+
+  it('deve retornar um erro',async() => {
+    const expectedError = new Error('You must provide an url');
+    const result = await fetchProducts();
+    expect(result).toEqual(expectedError); 
+  });
 });

71c8fb2ac9b9856ef128610b1898ddea19b77eda
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-11-13 16:47:12 -0300

    Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro 'cartItems' e o segundo sendo o valor passado como argumento para saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -7,16 +7,7 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('4 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
   it('Teste se, ao executar saveCartItems com o argumento <ol><li>Item</li></ol>, o método localStorage.setItem é chamado;', () => {
     saveCartItems('<ol><li>Item</li></ol>')
-    
   })
 });
-
-/* 
-it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', () => {
-    fetchItem('MLB1615760527')
-    expect(fetch).toHaveBeenCalled();
-  });
-   */

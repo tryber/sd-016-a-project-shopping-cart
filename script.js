bafc454d52e3afba081b914be9c8e9bde42d55dd
neguinho <victor_maanoel@Outlook.com>
2021-11-12 23:46:26 -0400

    requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+// REQUISITO 2 FEITO NA SALA COM A FUMAGALLI(COM A INICIATIVA DE AJUDAS OS ALUNOS EM RECUPERAÇÃO <3)
+
+const listOl = document.querySelector('ol');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+
 }
 
 function createCustomElement(element, className, innerText) {
@@ -12,33 +21,36 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-  
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  // Adicione o elemento retornado da função createProductItemElement(product) como filho do elemento <section class="items">.
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function createProductItemElement(itemObj) {
+  const { sku, name, image } = itemObj; 
+  const section = document.createElement('section');
+  section.className = 'item';
+  
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  button.addEventListener('click', () => 
+  listOl.appendChild(createCartItemElement(itemObj)));
+  section.appendChild(button);
+  // const button = document.querySelector('.item__add');
+  // button.addEventListener('click', (event) => {
+  //   const { target } = 
+  // })
+
+  return section;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
 }
 
 async function searchProduct(product) {
@@ -60,8 +72,9 @@ async function searchProduct(product) {
 //   const require = await fetchItem();
 //   const button = document.querySelector('item__add');
 //   const getOl = document.querySelector('.cart__items');
-
-// return   
+//   console.log(button)
+// }
+// addCartItem()
    
 // }
 
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -9,10 +9,12 @@ describe('1 - Teste a função fecthProducts', () => {
   it('Testa se fetchProducts é uma função;', () => {
     expect(typeof fetchProducts).toBe('function')
   });
+  
   it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada;', () => {
    fetchProducts('computador');
    expect(fetch).toHaveBeenCalled()
   });
+
   it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador";', () => {
     fetchProducts('computador')
     const URL = "https://api.mercadolibre.com/sites/MLB/search?q=computador";

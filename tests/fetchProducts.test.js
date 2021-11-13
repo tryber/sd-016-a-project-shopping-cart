110b96eade7be6859d506b7fe29bc9591cdcfbf1
Gustavo Ellwanger <ellwangerinvestimentos@gmail.com>
2021-11-13 16:54:39 -0300

    Corrige arquivo de testes fetchProduct

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,3 +1,7 @@
+const fetchSimulator = require('../mocks/fetchSimulator');
+const { fetchProducts } = require('../helpers/fetchProducts');
+const computadorSearch = require('../mocks/search');
+
 window.fetch = jest.fn(fetchSimulator);
 
 describe('1 - Teste a função fecthProducts', () => {

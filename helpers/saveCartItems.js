8e38d248797a7fff269a562999d33bbfa8f800b0
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-11-13 16:10:43 -0300

    Carregue o carrinho de compras através do LocalStorage ao iniciar a página - saveCartItems e getSavedCartItemms

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,7 +1,13 @@
-const saveCartItems = () => {
-  // seu código aqui
-};
+/* A função saveCartItems deve salvar os itens do carrinho de compras no localStorage, em uma chave denominada cartItems. Todas as adições e remoções devem ser abordadas para que a lista esteja sempre atualizada. */
 
+const getSavedCartItems = (item) => {
+   // seu código aqui
+   localStorage.setItem('cartItems', item);
+ };
+ 
 if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
+  module.exports = getSavedCartItems;
 }
+
+// localStorage.setItem("firstname", "Adam") //salva uma entrada com a key = "firstname" e value = "Adam"
+// localStorage.setItem("lastname", "Smith") //salva uma entrada com a key = "lastname" e value = "Smith"

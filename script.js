fe389023f11f242fd52e4500422f2178164a4f8a
Mauricio Moraes Roberti <mroberti@uol.com.br>
2021-11-22 19:53:48 -0300

    Adiciona Somea o valor total dos itens do carrinho de compras e cria botão limpar carrinho. Ajuda para resolver Matheus Guedes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Para criar essas variáveis recebi ajuda do Matheus Guedes em um 1:1
+let total = 0;
+const totalPrice = document.querySelector('.total-price');
+const limpar = document.querySelector('.empty-cart');
+const listaOl = document.querySelector('ol');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -6,6 +12,8 @@ function createProductImageElement(imageSource) {
 }
 function cartItemClickListener(event) {
   event.target.remove();
+  event.target.classList.remove('cart__item');
+  soma(Number(-event.target.classList));
 }
 
 function createCustomElement(element, className, innerText) {
@@ -19,9 +27,9 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  li.classList.add(salePrice);
   return li;
 }
-const listaOl = document.querySelector('ol');
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -34,16 +42,32 @@ function createProductItemElement({ sku, name, image }) {
     const result = await fetchItem(sku);
     const lista = await createCartItemElement(result);
     listaOl.appendChild(lista);
+    console.log(result.price);
+    soma(result.price);
     getSavedCartItems();
   });
 
   section.appendChild(btn);
   return section;
 }
+// Para está função recebi ajuda do Matheus Guedes em um 1:1
+function soma(price) {
+  total += price;
+  totalPrice.innerText = total;
+  console.log(totalPrice);
+}
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+// Para está função recebi ajuda do Matheus Guedes em um 1:1
+function limparCarrinho() {
+  listaOl.innerHTML = '';
+  total = 0;
+  totalPrice.innerText = '0.00';
+}
+
+limpar.addEventListener('click', limparCarrinho);
 
 // Para carregar a página
 // async await -> Será uma função assícrona e vai esperar por algo
@@ -67,5 +91,5 @@ async function searchProducts(product) {
 
 window.onload = () => {
   searchProducts('computador');
-  listaOl.innerHTML = getSavedCartItems;
+  // listaOl.innerHTML = getSavedCartItems;
 };
